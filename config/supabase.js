9df3464201de26216a27c5c4205683535b47e0dd
benjaminboussemart74-design <benjamin.boussemart74@gmail.com>
2025-10-27 12:34:28 +0100

    Front: affichage automatique des ministres délégués + réintégration toolbar et script; corrections d’initialisation et de rendu

--- config/supabase.js
+++ config/supabase.js
@@ -1,9 +1,6 @@
-// Configuration Supabase pour Gouvernement Lecornu II
-// Remplacez les valeurs par vos propres identifiants Supabase
+
 
 window.SUPABASE_URL = "https://yyquczbuhxbfnefwgxxc.supabase.co";
 window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl5cXVjemJ1aHhiZm5lZndneHhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk3ODI5NjcsImV4cCI6MjA3NTM1ODk2N30.0IDUEP96IQQ4Ylz-YoTY1UzoySLT9Wq2YLC2zAb7jds";
 
-// Vous pouvez obtenir ces valeurs dans l'interface Supabase :
-// - SUPABASE_URL : URL du projet
-// - SUPABASE_ANON_KEY : Clé publique (anon) du projet
+
